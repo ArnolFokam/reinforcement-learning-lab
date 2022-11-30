3d88ffc4bcf8f99c8dc226dd6d872344a0470461
Arnol Fokam <arnolfokam23@gmail.com>
2022-11-30 20:23:21 +0200

    add runner

---
+++ rl_lab/runner.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+def run_bandits_experiments(agents_specs, environment, timesteps, num_actions, num_experiments, line_scale):
+        
+    agents = {
+        agent: {
+            "params": specs["params"], #  parameters of the learning agent
+            "color": specs["color"], # color for visualization
+            "average_reward_per_timesteps": np.zeros(timesteps),
+            "average_reward_per_actions": np.zeros(num_actions),
+            } for agent, specs in agents_specs.items()
+        }
+    
+    for idx in range(1, num_experiments + 1):
+    
+        env = environment(line_scale)
+
+        # create available 10 jumps such that 
+        # there exists only few possible jumps 
+        actions = np.random.randint(-line_scale, line_scale, num_actions)
+        
+        # sort action in increasing reward 
+        rewards_per_actions = np.vectorize(lambda action: env.get_reward(action))(actions)
+        sorted_actions_index = np.argsort(rewards_per_actions)
+        actions = actions[sorted_actions_index]
+        
+        for name in agents:
+            # create a new agent for each experiment
+            # all agent interact on the same environment (but not at once).
+            agent = agents_specs[name]["class"](list(actions), timesteps, env, **agents[name]["params"])
+            agent.explore()
+            
+            # update metrics average over experiments
+            agents[name]["average_reward_per_timesteps"] = agents[name]["average_reward_per_timesteps"] + (1.0 / idx) * (agent.rewards_per_timesteps - agents[name]["average_reward_per_timesteps"])
+            agents[name]["average_reward_per_actions"] = agents[name]["average_reward_per_actions"] + (1.0 / idx) * (agent.expected_rewards - agents[name]["average_reward_per_actions"])
+            
+    return agents
+        
