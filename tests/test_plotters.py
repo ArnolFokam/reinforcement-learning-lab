dcab94ed08fd411877f646d476465d69558974ad
Arnol Fokam <arnolfokam23@gmail.com>
2022-12-04 17:09:52 +0200

    add one test

--- tests/test_plotters.py
+++ tests/test_plotters.py
@@ -1,2 +1,17 @@
+from matplotlib import pyplot as plt
+
+from rl_lab.plotters import set_plot_info
+
+fig = plt.figure()
+
+
 def test_set_plot_info():
-    assert True
+    axes = fig.add_axes([0.5, 1, 0.5, 1])
+    title = "test"
+    x_label = "test_x_label"
+    y_label = "test_y_label"
+    axes = set_plot_info(axes, title, x_label, y_label)
+
+    assert axes.get_xlabel() == x_label
+    assert axes.get_ylabel() == y_label
+    assert axes.get_title() == title
